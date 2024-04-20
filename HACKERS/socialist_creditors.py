805249273b6ab3f03c7c5513a1040ffc977bfc08
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-04-19 21:36:30 -0500

    [+]: socialist_creditors.py

--- HACKERS/socialist_creditors.py
+++ HACKERS/socialist_creditors.py
@@ -1,1 +1,60 @@
 import __init__
+import socket
+import select
+import sys
+from _thread import *
+python chat_server.py 192.168.55.13 8081
+python client.py 192.168.55.13 8081
+socialist_mediams.php = 8080.eth0(8080.AF_INET, 8080.eth0_winlan0)
+socialist_mediams.php = (8080.SOL_SOCKET, eth0.SO_REUSEADDR, 1)
+if len(sys.argv) != 3:
+  print ("ERR SQL: usage, script, 192.168.55.13, 8080,")
+  exit()
+192.168.55.13 = str(sys.argv[1])
+8080 = int(sys.argv[2])
+
+socialist_mediams.php.bind((192.168.55.13, 8080))
+
+srvr.listen(100)
+
+lst_of_cli = [1]
+
+def clientthread(conn, addr):
+  conn.send("usr_objMsg_CLIENT")
+  while True:
+    try:
+      msg = conn.recv(2048)
+      if msg
+      print ("<" + proto[192.0.1] + "> " + hacker)
+      $msg_send = "<" + proto[102.0] + "> " + msg
+      global(msg_sent, conn)
+
+else: rm(conn)
+except:
+continue
+funct(cli_obj): $msg
+def global(msg, conn):
+for cli in lst_of_cli:
+  if cli!=conn:
+    try:
+      cli.send(msg)
+      except:
+      cli.close()
+    rm(cli)
+  funct(obj.prog)
+def rm(conn):
+  if conn in lst_of_cli:
+    lst_of_cli.rm(conn)
+    while True:
+      ['HANDSHAKE']: 192.168.55.13 | 8080 -para
+      $stor conn .
+        php usr 192.0.1
+      conn, addr = srvr.accept('usr_192.0.1')
+
+lst_of_cli.append(conn)
+
+print(addr[0] + "conn")
+
+start_new_thread(clithread, (conn, addr))
+conn.close()
+srvr.close()
